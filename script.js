9d30b1bb99bb01eac9f2d924fb33fe0bb8e4bf0b
ikenna nwaka <inwaka39@gmail.com>
2025-07-03 20:08:17 +0200

    endingIndex

--- script.js
+++ script.js
@@ -16,5 +16,6 @@ fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
   });
   const fetchMoreAuthors = () => {
       startingIndex += 8;
+      endingIndex += 8;
      
   }
